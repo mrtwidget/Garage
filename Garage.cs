fa93e9832214582a821b891ebd2b21abd0611f77
Nexis <mrtwidget@gmail.com>
2020-01-06 14:03:55 -0500

    Initial Upload

---
+++ Garage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Rocket.Unturned;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using Rocket.Unturned.Chat;
+using Rocket.API;
+using Rocket.API.Collections;
+using SDG.Unturned;
+using UnityEngine;
+using Logger = Rocket.Core.Logging.Logger;
+using Steamworks;
+
+namespace NEXIS.Garage
+{
+    public class Garage : RocketPlugin<GarageConfiguration>
+    {
+        #region Fields
+
+        public static Garage Instance;
+
+        #endregion
+
+        #region Overrides
+
+        protected override void Load()
+        {
+            Instance = this;
+
+            U.Events.OnPlayerConnected += Events_OnPlayerConnected;
+            U.Events.OnPlayerDisconnected += Events_OnPlayerDisconnected;
+
+            Logger.Log("Loaded!", ConsoleColor.DarkGreen);
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log("Unloaded!", ConsoleColor.Yellow);
+        }
+
+        public override TranslationList DefaultTranslations
+        {
+            get
+            {
+                return new TranslationList() {
+                    {"garage_disabled", "Garage is currently unavailable"}
+                };
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        public void Events_OnPlayerConnected(UnturnedPlayer player)
+        {
+        }
+
+        public void Events_OnPlayerDisconnected(UnturnedPlayer player)
+        {
+        }
+
+        #endregion
+
+        public void FixedUpdate()
+        {
+            if (Instance.State != PluginState.Loaded) return;
+        }
+    }
+}
---
+++ GarageConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Rocket.API;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System;
+
+namespace NEXIS.Garage
+{
+    public class GarageConfiguration : IRocketPluginConfiguration
+    {
+        public bool Debug;
+
+        public void LoadDefaults()
+        {
+            Debug = true;
+        }
+    }
+}
